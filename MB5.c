cc98572ff19101aa8c28160ac04b0d89e7e5b4b9
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-11-27 08:51:19 +0200

    Update MB5.c

--- MB5.c
+++ MB5.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int G[20][20] , distance[20] , inSet[20] , q[20] , parent[20] ;
-void print( int V )
-{
-    int i ;
-    for ( i = 0 ; i < V ; i++ )
-        printf("i = %d parent = %d distance from source = %d \n", i + 1 , parent[i] , distance[i] ) ;
-}
+
 int Q( int V )
 {
     int sum = 0 , i ;
@@ -47,25 +42,4 @@ void dijkstra ( int S , int V )
     }
 
     print(V);
-}
-int main()
-{
-    int V , i , j , S;
-    printf ( "Enter no. of vertices: " ) ;
-    scanf ( "%d" , &V ) ;
-
-    printf ( "Enter graph in matrix form:\n" ) ;
-    for ( i = 0 ; i < V ; i++ )
-    {
-        for( j = 0 ; j < V ; j++ )
-            scanf( "%d" , &G[i][j] );
-    }
-    for ( i = 0 ; i < V ; i++ )
-        distance[i] = 1000 , inSet[i] = 0 , q[i] = 1 , parent[i] = -1 ;
-
-    printf ( "Enter the source vertex: " ) ;
-    scanf ( "%d" , &S ) ;
-    distance[ S - 1 ] = 0 ;
-    dijkstra ( S , V ) ;
-    return 0;
 }

7eef6abce5fde616d9a6c70c26e9fb462d83c15b
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-12-01 20:29:34 +0200

    Update MB5.c

--- MB5.c
+++ MB5.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-int G[20][20] , distance[20] , inSet[20] , q[20] , parent[20] ;
 
+int G[100][100] , distance[1000] , inSet[1000] , q[1000];
+void print( int V )
+{
+    int i ;
+    for ( i = 0 ; i < V ; i++ )
+    if(i!=29)
+    {
+        printf("Vertex V%d  distance from source = %d \n", i , distance[i] ) ;
+    }
+    else
+    {
+        printf("Vertex V* distance from source = %d \n", distance[i] ) ;
+    }
+}
 int Q( int V )
 {
     int sum = 0 , i ;
@@ -20,7 +32,7 @@ int extractMin( int V )
     q[idx] = 0 ;
     return idx ;
 }
-void dijkstra ( int S , int V )
+void dijkstra ( int V )
 {
     int u , i , check_empty = Q(V);
 
@@ -35,11 +47,32 @@ void dijkstra ( int S , int V )
             if( G[u][i] > 0 )
             {
                 if( distance[u] + G[u][i] < distance[i] )
-                    distance[i] = distance[u] + G[u][i] , parent[i] = u + 1 ;
+                    distance[i] = distance[u] + G[u][i] ;
             }
         }
         check_empty = Q(V);
     }
 
     print(V);
+}
+int main()
+{
+    int V , i , j , S;
+    printf ( "Enter no. of vertices: " ) ;
+    scanf ( "%d" , &V ) ;
+
+    printf ( "Enter graph in matrix form:\n" ) ;
+    for ( i = 0 ; i < V ; i++ )
+    {
+        for( j = 0 ; j < V ; j++ )
+            scanf( "%d" , &G[i][j] );
+    }
+    for ( i = 0 ; i < V ; i++ )
+        distance[i] = 1000 , inSet[i] = 0 , q[i] = 1 ;
+
+    printf ( "Enter the source vertex: " ) ;
+    scanf ( "%d" , &S ) ;
+    distance[ S  ] = 0 ;
+    dijkstra ( V ) ;
+    return 0;
 }
